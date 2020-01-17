7ce609b6f49059e0bb041b626fde966699fb64b6
mmden <mmn@outlook.com.br>
2020-01-16 23:01:03 -0300

    Iniciando o app mobile

---
+++ mobile/src/pages/Main.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
+import {requestPermissionsAsync, getCurrentPositionAsync} from 'expo-location';
+
+// To Do: Continuar em 55:59
+function Main() {
+    const [currentRegion, setCurrentRegion] = useState(null);
+
+    useEffect(() => {
+        async function loadInitPosition() {
+            const {granted} = await requestPermissionsAsync();
+
+            if(granted){
+                const {coords} = await getCurrentPositionAsync({
+                    enableHighAccuracy: true,
+                });
+
+                const {latitude, longitude} = coords;
+
+                setCurrentRegion({
+                    latitude,
+                    longitude,
+                    latitudeDelta:  0.04,
+                    longitudeDelta: 0.04 ,
+                });
+            }
+        }
+
+        loadInitPosition();
+
+    }, []);
+
+    if(!currentRegion){
+        return null;
+    }
+
+    return <MapView initialRegion={currentRegion} style={styles.map} />
+}
+
+const styles = StyleSheet.create({
+    map: {
+        flex: 1,
+
+    }
+})
+
+export default Main;
